98a27930f70951eadde0e0999762ed66089dbeda
Ali <edualihabib@gmail.com>
2024-02-12 23:31:11 +0300

    add auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\PostsController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,19 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [PostsController::class,'index'])->name('posts.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/posts/creat', [PostsController::class,'creat'])->name('posts.creat');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/posts/{post}', [PostsController::class,'show'])->name('posts.show');
-
-Route::post('/posts', [PostsController::class,'store'])->name('posts.store') ;
-
-Route::get('/posts/{post}/edit',[PostsController::class,'edit'])->name('posts.edit');
-
-Route::put('/posts/{post}',[PostsController::class,'update'])->name('posts.update');
-
-Route::delete('/posts/{post}',[PostsController::class,'destroy'])->name('posts.destroy');
-
-// 1- structure change for database (creat table , edit column , remove column)
-// 2- operations on database (insert record ,edit record ,delete record)
+require __DIR__.'/auth.php';
